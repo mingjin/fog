a4fa22d58dba0656d1c58cc8dfa2849d5576d427
superzbo <zbo2@qq.com>
2015-02-28 14:02:56 +0800

    refactor nic create using vm_reconfig_hardware

--- lib/fog/vsphere/requests/compute/create_nic.rb
+++ lib/fog/vsphere/requests/compute/create_nic.rb
@@ -5,7 +5,6 @@ module Fog
         def create_nic options = { }
           raise ArgumentError.new('Invalid vm') if options['instance_uuid'].nil?
           raise ArgumentError.new('Invalid network') if options['network'].nil?
-          vm = get_vm_ref(options['instance_uuid'])
           nic = RbVmomi::VIM::VirtualE1000(
               :backing => RbVmomi::VIM::VirtualEthernetCardNetworkBackingInfo(:deviceName => options['network']),
               :connectable => RbVmomi::VIM::VirtualDeviceConnectInfo(
@@ -15,8 +14,8 @@ module Fog
           )
           nic[:key]=options['key'] unless options['key'].nil?
           nic[:deviceInfo]= RbVmomi::VIM::VirtualDeviceConnectInfo(:label=>options['lable'],:summary=>options['network']) unless options['label'].nil?
-          vm_cfg={:deviceChange=>[{:operation=>:add,:device=>nic}]}
-          vm.ReconfigVM_Task(:spec => vm_cfg).wait_for_completion
+          hardware_spec={:deviceChange=>[{:operation=>:add,:device=>nic}]}
+          vm_reconfig_hardware('instance_uuid' => options['instance_uuid'], 'hardware_spec' => hardware_spec )
         end
       end
     end
