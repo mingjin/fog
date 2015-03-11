dc7af2f3341b72875687957359f6542226ad1814
Haisong Zou <zouhaisong@gmail.com>
2015-03-11 19:09:45 +0800

    Fix vsphere list_nic nil error

--- lib/fog/vsphere/requests/compute/list_nics.rb
+++ lib/fog/vsphere/requests/compute/list_nics.rb
@@ -6,7 +6,7 @@ module Fog
           options[:folder] ||= options['folder']
           search_filter = { :uuid => options['instance_uuid'], 'vmSearch' => true, 'instanceUuid' => true }
           vm_mob_ref = @connection.searchIndex.FindAllByUuid(search_filter).first
-          return [] unless vm_mob_ref
+          return {"nics"=>[]} unless vm_mob_ref
 
           nics = vm_mob_ref.config.hardware.device.find_all{|d| d.is_a?(RbVmomi::VIM::VirtualEthernetCard)}
           ipmap_array=vm_mob_ref.guest.net.map{|n| [n.deviceConfigId,n.ipAddress]}
